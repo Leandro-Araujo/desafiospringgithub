8dc83ca531dcbf4a82934802647dce195c08c298
Leandro <leandro_sy@hotmail.com>
2024-06-07 20:42:43 -0300

    first commit

---
+++ src/main/java/com/github/leandro/desafiospringgithub/DesafiospringgithubApplication.java
@@ -0,0 +1,13 @@
+package com.github.leandro.desafiospringgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiospringgithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiospringgithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/leandro/desafiospringgithub/DesafiospringgithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.leandro.desafiospringgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiospringgithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
